43c36155bd647aa841fc5f7ee67672ff33e2b66d
Manuel Bugallo <marvinody@gmail.com>
2019-10-01 09:04:52 -0400

    chore: skeleton of linked list

---
+++ Data_Structures/JavaScript/LinkedList.js
@@ -0,0 +1,38 @@
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  prepend() {
+
+  }
+
+  append() {
+
+  }
+
+  insert(value, idx) {
+
+  }
+
+  remove(idx) {
+
+  }
+
+  contains() {
+
+  }
+
+  size() {
+
+  }
+
+}
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = null;
+  }
+}
