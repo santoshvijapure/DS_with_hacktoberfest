9da635b75a80c86bc04a88f156bfeada7f9dfbef
portkey2112 <spoorthi2112@gmail.com>
2019-10-26 23:51:06 +0530

    Removed unwanted comments and renamed the function appropriately

--- recursive_insertion_sort.py
+++ recursive_insertion_sort.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 26 23:14:46 2019
-
-@author: 1000259877
-"""
 
 arr = [2,0,9,8,7,6,5,2,3,4,3,2,1]
-#arr = [9,8]
 
-def inssort(n):
+def rec_inssort(n):
     if(n==0):
         return
     inssort(n-1)
@@ -20,7 +14,7 @@ def inssort(n):
     arr[j+1] = num
     
 print("Before sorting :", arr)
-inssort(len(arr) - 1)
+rec_inssort(len(arr) - 1)
 print("After sorting", arr)
         
     
