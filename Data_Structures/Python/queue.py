28b753a8e3182966d009ea179de6945f8ecd3f67
Pratyusha Thumiki <pratyusha.thumiki@gmail.com>
2019-10-29 08:47:28 +0530

    Implemented queue in Python
    type:new feature
    Implemented queue using Python

---
+++ Data_Structures/Python/queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+	
+    def enqueue(self, item):
+           self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+q=Queue()
+
+q.enqueue(9)
+q.enqueue('String here')
+q.enqueue(True)
+print(q.size())
